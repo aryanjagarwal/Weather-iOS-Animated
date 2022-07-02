5a8b429fe6132d72fb2d3918f0bc6be6c1da11b5
aryanjagarwal <68804795+aryanjagarwal@users.noreply.github.com>
2022-07-02 08:10:12 +0530

    Initial Commit

---
+++ Weather-iOS-Animated/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather-iOS-Animated
+//
+//  Created by Aryan Jagarwal on 02/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather-iOS-Animated/Weather_iOS_AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_iOS_AnimatedApp.swift
+//  Weather-iOS-Animated
+//
+//  Created by Aryan Jagarwal on 02/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_iOS_AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
